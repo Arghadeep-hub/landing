ab1a63c31dce0d973841c7f356379b7da49baf30
Arghadeep Mallick <66899260+Arghadeep-hub@users.noreply.github.com>
2021-06-12 19:01:45 +0530

    update members area

--- src/component/Member.js
+++ src/component/Member.js
@@ -4,7 +4,7 @@ const Member = [
         name: "Sourav Karmakar",
         position: "Head",
         image: "img/sourav_karmakar.jpg",
-        comment: "Measuring programming progress by lines of code is like measuring aircraft building progress by weight.",
+        comment: "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
         Whatsapp: "",
         facebook: "https://facebook.com",
         linkedin: "https://linkedin.com",
@@ -15,7 +15,7 @@ const Member = [
         name: "Arghdeep Mallick",
         position: "Associate Heade",
         image: "img/Arghadeep.jpg",
-        comment: "If debugging is the process of removing software bugs, then programming must be the process of putting them in and It’s not a bug. It’s an undocumented feature!",
+        comment: "Give a man a program, frustrate him for a day. Teach a man to program, frustrate him for a lifetime.",
         Whatsapp: "https://whatsapp.com",
         facebook: "https://facebook.com",
         linkedin: "https://linkedin.com",
@@ -23,6 +23,28 @@ const Member = [
     },
     {
         id: 3,
+        name: "Sumit Sarkar",
+        position: "Backend Heade",
+        image: "img/Sumit_srkr.jpg",
+        comment: "If debugging is the process of removing software bugs, then programming must be the process of putting them in and It’s not a bug. It’s an undocumented feature!",
+        Whatsapp: "https://whatsapp.com",
+        facebook: "https://facebook.com",
+        linkedin: "https://linkedin.com",
+        github: "https://github.com"
+    },
+    {
+        id: 4,
+        name: "Pushpa Kumari",
+        position: "Frontend Head",
+        image: "https://i.ibb.co/chV28BZ/1613664803212.jpg",
+        comment: "Things aren’t always #000000 and #FFFFFF",
+        Whatsapp: "https://whatsapp.com",
+        facebook: "https://www.facebook.com/profile.php?id=100028588115288",
+        linkedin: "https://www.linkedin.com/in/pushpa-kumari-62491516a/",
+        github: "https://github.com/pushpakumari5117"
+    },
+    {
+        id: 5,
         name: "Sudhanshu Kumar",
         position: "project manager",
         image: "img/sudhanshu.jpeg",
@@ -33,7 +55,7 @@ const Member = [
         github: "https://github.com"
     },
     {
-        id: 4,
+        id: 6,
         name: "Suman Ojha",
         position: "Project Manager",
         image: "img/SumanOjha.png",
@@ -44,7 +66,7 @@ const Member = [
         github: "https://github.com"
     },
     {
-        id: 5,
+        id: 7,
         name: "Md. Ebrahim",
         position: "Developer",
         image: "img/ebrahim.jpeg",
@@ -55,7 +77,7 @@ const Member = [
         github: "https://github.com"
     },
     {
-        id: 6,
+        id: 8,
         name: "Aman Sharma",
         position: "Developer",
         image: "img/amanSharma.jpeg",
